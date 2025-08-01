33c828671d4f08228603bf9a702279c9b8774d07
lemancebrayilova <cbrayilovalman33@gmail.com>
2025-08-01 22:12:33 +0300

    Render movies from JSON API

---
+++ app.js
@@ -0,0 +1,26 @@
+let url = "https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies";
+let movies = document.getElementsByClassName('movies')[0];
+
+fetch(url).then((response) => {
+    return response.json();
+}).then((result) => {
+    // console.log(result);
+    result.forEach(element => {
+        const { Title, Year, Runtime, Poster } = element;
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+        <img src="${Poster}" alt="">
+            <div class="content">
+                <h3>${Title}</h3>
+                <div class="year_time">
+                    <h6>${Year}</h6>
+                    <h6>${Runtime}</h6>
+                    <h6>7/10</h6>
+                </div>
+            </div>
+        `;
+
+        movies.appendChild(card);
+    });
+})
